bc077a5650701916659878fd3b5aa22e7c1dd43b
alesalvarredy <asalvarredyc@gmail.com>
2022-11-12 01:43:43 -0300

    add burguer menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,27 @@
 const header = document.getElementById("header");
+const navBurguer = document.querySelector(".nav_burguer");
+const navBurguerLine1 = document.querySelector(".nav_burguer-line-1");
+const navBurguerLine2 = document.querySelector(".nav_burguer-line-2");
+const navBurguerLine3 = document.querySelector(".nav_burguer-line-3");
+const itemContainer = document.querySelector(".items_container");
+const menuItems = document.querySelectorAll(".menuItems");
+
+const showNavMenu = () => {
+    navBurguerLine1.classList.toggle("active");
+    navBurguerLine2.classList.toggle("active");
+    navBurguerLine3.classList.toggle("active");
+    itemContainer.classList.toggle("active");
+}
+
+navBurguer.addEventListener("click", () => {
+    showNavMenu();
+})
+
+menuItems.forEach((items)=>{
+    items.addEventListener("click", () => {
+        showNavMenu();
+    } )
+})
 
 window.addEventListener("scroll", ()=>{
     if(window.scrollY){
@@ -7,3 +30,4 @@ window.addEventListener("scroll", ()=>{
         header.classList.remove("active");
     }
 })
+
